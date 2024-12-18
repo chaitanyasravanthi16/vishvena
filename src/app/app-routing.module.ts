ed63c6b70cf0409e527444d9e93f67ed47bc8a15
chaitanyasravanthi16 <chaitanyasravanthi2002@gmail.com>
2024-12-18 21:10:55 +0530

    my first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NavbarComponent,
+    children:[
+      {path:'expertise',component:ExpertiseComponent},
+      {path:'footer',component:FooterComponent},
+      {path:'contact',component:ContactComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ExpertiseComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
